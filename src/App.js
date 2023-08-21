a6da37faf32e3b03cec7d4b28e9a1c74ab179100
Fahimul Islam <57781871+Fahim047@users.noreply.github.com>
2023-08-21 22:38:19 +0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello Good People !!!</h1>
+      <h2>
+        This repo contains the practice projects of udemy react js course by
+        Jonas. Change the branch to see the projects.
+      </h2>
+    </div>
+  );
+}
