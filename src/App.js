5bc9e3a2653ae387b76ca8ad24d73965a1a78495
Fahimul Islam <57781871+Fahim047@users.noreply.github.com>
2023-08-22 02:40:04 +0000

    rendered packinglist

--- src/App.js
+++ src/App.js
@@ -1,13 +1,63 @@
-import "./styles.css";
+import "./index.css";
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+];
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello Good People !!!</h1>
-      <h2>
-        This repo contains the practice projects of udemy react js course by
-        Jonas. Change the branch to see the projects.
-      </h2>
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
     </div>
+  );
+}
+
+function Logo() {
+  return <h1> Network Er Baire </h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for the trip?</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry.
+      </em>
+    </footer>
   );
 }
